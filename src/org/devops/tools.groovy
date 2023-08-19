578800a687dd426e2a6462fe708dc0951ecda065
youyou566 <63624092+youyou566@users.noreply.github.com>
2023-08-19 20:05:02 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -3,7 +3,7 @@ package org.devops
 def PrintMes(content){
   println(content)
 }
-//格式化输出
+//格式化输出(添加颜色)
 def PrintMes(value,clor){
   clors =  ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<<<\033[0m",
             'blue'  : "\033[47;34m ${value} \033[0m",
