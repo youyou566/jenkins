8322b9765d2db2be1470e4a54e65cfeb74823398
youyou566 <63624092+youyou566@users.noreply.github.com>
2023-08-19 21:36:49 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,8 +1,8 @@
 package org.devops
 //打印内容
-def PrintMes(content){
-  println(content)
-}
+//def PrintMes(content){
+//  println(content)
+//}
 //格式化输出(打印信息的方法ansiClor)
 def PrintMes(value,clor){
   clors =  ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<<<\033[0m",
